c1aa332c6563092296bb84e63c7c66344c8ccf8b
brysommer <brysommer1@gmail.com>
2025-09-02 15:36:02 +0300

    cards to CardId

--- models/api-users.js
+++ models/api-users.js
@@ -1,98 +1,85 @@
 import { Model, DataTypes } from "sequelize";
-import { sequelize } from './sequelize.js';
-import { logger } from '../logger/index.js';
-
+import { sequelize } from "./sequelize.js";
+import { logger } from "../logger/index.js";
 
 class Apiuser extends Model {}
-Apiuser.init({
+Apiuser.init(
+  {
     user_id: {
-        type: DataTypes.BIGINT,
-        allowNull: false,
-        unique: true
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      unique: true,
     },
     chat_id: {
-        type: DataTypes.BIGINT,
-        allowNull: false,
-        unique: true
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      unique: true,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     birthdaydate: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     phone: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true,
     },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: true
+    cardId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
-    cards: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    
-
-}, {
+  },
+  {
     freezeTableName: false,
     timestamps: true,
-    modelName: 'Apiusers',
-    sequelize
-});
+    modelName: "Apiusers",
+    sequelize,
+  }
+);
 
 const createNewApiUser = async (userData) => {
-    let res;
-    try {
-        res = await Apiuser.create({ ...userData });
-        res = res.dataValues;
-    } catch (err) {
-        logger.error(`Impossible to create user: ${err}`);
-    }
-    return res;
+  let res;
+  try {
+    res = await Apiuser.create({ ...userData });
+    res = res.dataValues;
+  } catch (err) {
+    logger.error(`Impossible to create user: ${err}`);
+  }
+  return res;
 };
 
 const updateApiUserByChatId = async (chat_id, updateParams) => {
-    const res = await Apiuser.update({ ...updateParams } , { where: { chat_id } });
-    if (res[0]) {
-        const data = await findApiUserByChatId(chat_id);
-        if (data) {
-            //logger.info(`Api user ${data.chat_id} updated`);
-            return data;
-        }
-        logger.info(`Api user ${chat_id} updated, but can't read result data`);
-    } 
-    return undefined;
-};
-
-
-const findApiUserByUserId = async (user_id) => {
-    const res = await Apiuser.findAll({ where: { user_id } });
-    if (res.length > 0) return res.map(el => el.dataValues);
-    return;
+  const res = await Apiuser.update({ ...updateParams }, { where: { chat_id } });
+  if (res[0]) {
+    const data = await findApiUserByChatId(chat_id);
+    if (data) {
+      //logger.info(`Api user ${data.chat_id} updated`);
+      return data;
+    }
+    logger.info(`Api user ${chat_id} updated, but can't read result data`);
+  }
+  return undefined;
 };
 
 const findAllUsers = async () => {
-    const res = await Apiuser.findAll({ where: {  } });
-    if (res.length > 0) return res.map(el => el.dataValues);
-    return;
+  const res = await Apiuser.findAll({ where: {} });
+  if (res.length > 0) return res.map((el) => el.dataValues);
+  return;
 };
 
-
 const findApiUserByChatId = async (chat_id) => {
-    const res = await Apiuser.findOne({ where: { chat_id: chat_id } });
-    if (res) return res.dataValues;
-    return res;
+  const res = await Apiuser.findOne({ where: { chat_id: chat_id } });
+  if (res) return res.dataValues;
+  return res;
 };
 
 export {
-    Apiuser,
-    createNewApiUser,
-    updateApiUserByChatId,
-    findApiUserByUserId,
-    findApiUserByChatId,
-    findAllUsers
+  Apiuser,
+  createNewApiUser,
+  updateApiUserByChatId,
+  findApiUserByChatId,
+  findAllUsers,
 };
--- modules/statistic/bot-users-statistic.js
+++ modules/statistic/bot-users-statistic.js
@@ -1,189 +1,216 @@
 import { bot } from "../../app.js";
 import { logger } from "../../logger/index.js";
 import { findAllUsers } from "../../models/api-users.js";
-import { copyUsersTransactionsByTheDay, getBotUserTotalbyTheDay } from "../../models/bot-transactions.js";
+import {
+  copyUsersTransactionsByTheDay,
+  getBotUserTotalbyTheDay,
+} from "../../models/bot-transactions.js";
 import { Card } from "../../models/cards.js";
 import { collectDailyStatistics } from "../../models/daily-statistic.js";
-import { getUsersTotalbyTheDay, getUsersTotalByWeek, getUsersTotalByMonth, getUsersTotalCurrentMonth } from "../../models/transactions.js";
+import {
+  getUsersTotalbyTheDay,
+  getUsersTotalByWeek,
+  getUsersTotalByMonth,
+  getUsersTotalCurrentMonth,
+} from "../../models/transactions.js";
 import { dataBot } from "../../values.js";
 
-
-
-
 const botUsersStatistic = async () => {
-    const users = await findAllUsers();
-    const usersWithTotals = [];
+  const users = await findAllUsers();
+  const usersWithTotals = [];
 
-    
+  for (let user of users) {
+    const cardId = user?.cardId;
+    let userTotal = 0;
 
-    for (let user of users) {
-        const cardId = user?.cards;
-        let userTotal = 0;
+    if (cardId) {
+      await copyUsersTransactionsByTheDay(cardId);
 
-        if (cardId) {            
+      userTotal = await getBotUserTotalbyTheDay(cardId);
 
-            await copyUsersTransactionsByTheDay(cardId);
+      const increment = await Card.increment("LitersPerDay", {
+        by: userTotal,
+        where: { cardId },
+      });
+    }
 
-            userTotal = await getBotUserTotalbyTheDay(cardId);
+    usersWithTotals.push({
+      id: user.id,
+      name: user.name,
+      birthdayDate: user.birthdayDate,
+      phone: user.phone,
+      cardId: cardId,
+      userTotal,
+    });
+  }
 
-            const increment = await Card.increment('LitersPerDay', {
-                by: userTotal,
-                where: { cardId }
-            });
+  await collectDailyStatistics();
 
-        }
+  // Логуюємо загальну кількість користувачів та воду
+  const usersQuantity = users.length;
+  const usersWaterTotal = usersWithTotals.reduce(
+    (sum, user) => sum + user.userTotal,
+    0
+  );
 
-        usersWithTotals.push({
-            id: user.id,
-            name: user.name,
-            birthdayDate: user.birthdayDate,
-            phone: user.phone,
-            cards: cardId,
-            userTotal
-        });
-    };
-
-    await collectDailyStatistics();
-
-    // Логуюємо загальну кількість користувачів та воду
-    const usersQuantity = users.length;
-    const usersWaterTotal = usersWithTotals.reduce((sum, user) => sum + user.userTotal, 0);
-
-    const summaryString = `Користувачів боту: ${usersQuantity},\nКількість налитої води, користувачами боту, за добу: ${usersWaterTotal.toFixed(0)} літрів.`;
-    bot.sendMessage(dataBot.topId, summaryString);
-    
+  const summaryString = `Користувачів боту: ${usersQuantity},\nКількість налитої води, користувачами боту, за добу: ${usersWaterTotal.toFixed(
+    0
+  )} літрів.`;
+  bot.sendMessage(dataBot.topId, summaryString);
 };
 
 const botWeeklyUsersStatistic = async () => {
-    const users = await findAllUsers();
-    const usersWithTotals = [];
+  const users = await findAllUsers();
+  const usersWithTotals = [];
 
-    for (let user of users) {
-        const cardId = user?.cards;
-        let userTotal = 0;
+  for (let user of users) {
+    const cardId = user?.cardId;
+    let userTotal = 0;
 
-        if (cardId) {
-            userTotal = await getUsersTotalByWeek(cardId);
-        }
-
-        usersWithTotals.push({
-            id: user.id,
-            name: user.name,
-            birthdayDate: user.birthdayDate,
-            phone: user.phone,
-            cards: cardId,
-            userTotal
-        });
+    if (cardId) {
+      userTotal = await getUsersTotalByWeek(cardId);
     }
 
-    // Сортуємо користувачів за userTotal у порядку спадання
-    const topUsers = usersWithTotals
-        .sort((a, b) => b.userTotal - a.userTotal)
-        .slice(0, 5); // Отримуємо топ-5
+    usersWithTotals.push({
+      id: user.id,
+      name: user.name,
+      birthdayDate: user.birthdayDate,
+      phone: user.phone,
+      cardId: cardId,
+      userTotal,
+    });
+  }
 
-    // Формуємо повідомлення для топ-5 користувачів
-    const topUsersMessage = topUsers.map(user => {
-        return `ID: ${user.id}, Імя: ${user.name}, ТЕЛ: ${user.phone}, Карта: ${user.cards}, Набрано: ${user.userTotal.toFixed(0)} літрів`;
-    }).join('\n');
+  // Сортуємо користувачів за userTotal у порядку спадання
+  const topUsers = usersWithTotals
+    .sort((a, b) => b.userTotal - a.userTotal)
+    .slice(0, 5); // Отримуємо топ-5
 
-    bot.sendMessage(dataBot.topId, `Топ 5 користувачів за тиждень:\n${topUsersMessage}`);
+  // Формуємо повідомлення для топ-5 користувачів
+  const topUsersMessage = topUsers
+    .map((user) => {
+      return `ID: ${user.id}, Імя: ${user.name}, ТЕЛ: ${user.phone}, Карта: ${
+        user.cardId
+      }, Набрано: ${user.userTotal.toFixed(0)} літрів`;
+    })
+    .join("\n");
 
-    // Логуюємо загальну кількість користувачів та воду
-    const usersQuantity = users.length;
-    const usersWaterTotal = usersWithTotals.reduce((sum, user) => sum + user.userTotal, 0);
+  bot.sendMessage(
+    dataBot.topId,
+    `Топ 5 користувачів за тиждень:\n${topUsersMessage}`
+  );
 
-    const summaryString = `Користувачів боту: ${usersQuantity},\nКількість налитої води, користувачами боту, за тиждень: ${usersWaterTotal.toFixed(0)} літрів.`;
-    bot.sendMessage(dataBot.topId, summaryString);
+  // Логуюємо загальну кількість користувачів та воду
+  const usersQuantity = users.length;
+  const usersWaterTotal = usersWithTotals.reduce(
+    (sum, user) => sum + user.userTotal,
+    0
+  );
+
+  const summaryString = `Користувачів боту: ${usersQuantity},\nКількість налитої води, користувачами боту, за тиждень: ${usersWaterTotal.toFixed(
+    0
+  )} літрів.`;
+  bot.sendMessage(dataBot.topId, summaryString);
 };
 
 const botMonthlyUsersStatistic = async () => {
-    const users = await findAllUsers();
-    const usersWithTotals = [];
+  const users = await findAllUsers();
+  const usersWithTotals = [];
 
-    for (let user of users) {
-        const cardId = user?.cards;
-        let userTotal = 0;
+  for (let user of users) {
+    const cardId = user?.cardId;
+    let userTotal = 0;
 
-        if (cardId) {
-            userTotal = await getUsersTotalByMonth(cardId);
-        }
-
-        usersWithTotals.push({
-            id: user.id,
-            name: user.name,
-            birthdayDate: user.birthdayDate,
-            phone: user.phone,
-            cards: cardId,
-            userTotal
-        });
+    if (cardId) {
+      userTotal = await getUsersTotalByMonth(cardId);
     }
 
-    // Сортуємо користувачів за userTotal у порядку спадання
-    const topUsers = usersWithTotals
-        .sort((a, b) => b.userTotal - a.userTotal)
-        .slice(0, 5); // Отримуємо топ-5
+    usersWithTotals.push({
+      id: user.id,
+      name: user.name,
+      birthdayDate: user.birthdayDate,
+      phone: user.phone,
+      cardId: cardId,
+      userTotal,
+    });
+  }
 
-    // Формуємо повідомлення для топ-10 користувачів
-    const topUsersMessage = topUsers.map(user => {
-        return `ID: ${user.id}, Імя: ${user.name}, ТЕЛ: ${user.phone}, Карта: ${user.cards}, Набрано: ${user.userTotal.toFixed(0)} літрів`;
-    }).join('\n');
+  // Сортуємо користувачів за userTotal у порядку спадання
+  const topUsers = usersWithTotals
+    .sort((a, b) => b.userTotal - a.userTotal)
+    .slice(0, 5); // Отримуємо топ-5
 
-    bot.sendMessage(dataBot.topId, `Топ 5 користувачів за місяць:\n${topUsersMessage}`);
+  // Формуємо повідомлення для топ-10 користувачів
+  const topUsersMessage = topUsers
+    .map((user) => {
+      return `ID: ${user.id}, Імя: ${user.name}, ТЕЛ: ${user.phone}, Карта: ${
+        user.cardId
+      }, Набрано: ${user.userTotal.toFixed(0)} літрів`;
+    })
+    .join("\n");
 
-    // Логуюємо загальну кількість користувачів та воду
-    const usersQuantity = users.length;
-    const usersWaterTotal = usersWithTotals.reduce((sum, user) => sum + user.userTotal, 0);
+  bot.sendMessage(
+    dataBot.topId,
+    `Топ 5 користувачів за місяць:\n${topUsersMessage}`
+  );
 
-    const monthlyAmountUpdate = await Card.update(
-        { LitersPerDay: 0 }, 
-        { where: {} }
-    );
+  // Логуюємо загальну кількість користувачів та воду
+  const usersQuantity = users.length;
+  const usersWaterTotal = usersWithTotals.reduce(
+    (sum, user) => sum + user.userTotal,
+    0
+  );
 
-    const summaryString = `Користувачів боту: ${usersQuantity},\nКількість налитої води, користувачами боту, за місяць: ${usersWaterTotal.toFixed(0)} літрів.`;
-    bot.sendMessage(dataBot.topId, summaryString);
+  const monthlyAmountUpdate = await Card.update(
+    { LitersPerDay: 0 },
+    { where: {} }
+  );
 
-
+  const summaryString = `Користувачів боту: ${usersQuantity},\nКількість налитої води, користувачами боту, за місяць: ${usersWaterTotal.toFixed(
+    0
+  )} літрів.`;
+  bot.sendMessage(dataBot.topId, summaryString);
 };
 
 const getPersonalRankMessage = async (cardId) => {
-    // Отримуємо всі карти, що належать користувачам
-    const cards = await Card.findAll();
+  // Отримуємо всі карти, що належать користувачам
+  const cards = await Card.findAll();
 
-    // Отримуємо список користувачів із їхнім рейтингом (LitersPerDay)
-    const cardsWithTotals = cards.map(card => ({
-        cardId: card.cardId,
-        litersPerDay: card.LitersPerDay
-    }));
+  // Отримуємо список користувачів із їхнім рейтингом (LitersPerDay)
+  const cardsWithTotals = cards.map((card) => ({
+    cardId: card.cardId,
+    litersPerDay: card.LitersPerDay,
+  }));
 
-    // Сортуємо карти за LitersPerDay у порядку спадання
-    cardsWithTotals.sort((a, b) => b.litersPerDay - a.litersPerDay);
+  // Сортуємо карти за LitersPerDay у порядку спадання
+  cardsWithTotals.sort((a, b) => b.litersPerDay - a.litersPerDay);
 
-    // Знаходимо місце картки у рейтингу
-    const cardIndex = cardsWithTotals.findIndex(card => card.cardId === cardId);
-    const card = cardsWithTotals[cardIndex];
+  // Знаходимо місце картки у рейтингу
+  const cardIndex = cardsWithTotals.findIndex((card) => card.cardId === cardId);
+  const card = cardsWithTotals[cardIndex];
 
-    let rankMessage = '';
+  let rankMessage = "";
 
-    if (!card || card.litersPerDay === 0) {
-        rankMessage = `❌ *Рейтинг:* Ви не набрали жодного літра цього місяця.`;
-    } else if (cardIndex < 10) {
-        rankMessage = `🏆 *Рейтинг:* Ви *#${cardIndex + 1}* у рейтингу користувачів цього місяця!`;
-    } else {
-        const betterThanPercent = ((cards.length - cardIndex) / cards.length * 100).toFixed(1);
-        rankMessage = `📈 *Рейтинг:* Ви набрали більше води, ніж *${betterThanPercent}%* користувачів.`;
-    }
+  if (!card || card.litersPerDay === 0) {
+    rankMessage = `❌ *Рейтинг:* Ви не набрали жодного літра цього місяця.`;
+  } else if (cardIndex < 10) {
+    rankMessage = `🏆 *Рейтинг:* Ви *#${
+      cardIndex + 1
+    }* у рейтингу користувачів цього місяця!`;
+  } else {
+    const betterThanPercent = (
+      ((cards.length - cardIndex) / cards.length) *
+      100
+    ).toFixed(1);
+    rankMessage = `📈 *Рейтинг:* Ви набрали більше води, ніж *${betterThanPercent}%* користувачів.`;
+  }
 
-    return rankMessage;
+  return rankMessage;
 };
 
-
-
-
-
-export  {
-    botUsersStatistic,
-    botWeeklyUsersStatistic,
-    botMonthlyUsersStatistic,
-    getPersonalRankMessage,
-} 
-
+export {
+  botUsersStatistic,
+  botWeeklyUsersStatistic,
+  botMonthlyUsersStatistic,
+  getPersonalRankMessage,
+};
