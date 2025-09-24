615bbae4e4cc3f23a75c053dae90ac8603b2af0a
brysommer <brysommer1@gmail.com>
2025-09-24 17:59:50 +0300

    transaction create by API

---
+++ controllers/createUserTransaction.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { logger } from "../logger";
+
+export const createUserTransaction = async ({
+  device,
+  date,
+  waterRequested,
+  waterFullfilled,
+  cashPaymant,
+  cardPaymant,
+  onlinePaymant,
+  paymantChange,
+  cardId,
+}) => {
+  try {
+    const create = await axios.post("http://localhost/transactions/create", {
+      device,
+      date,
+      waterRequested,
+      waterFullfilled,
+      cashPaymant,
+      cardPaymant,
+      onlinePaymant,
+      paymantChange,
+      cardId,
+    });
+
+    logger.info(
+      "User trasaction already created. Transaction ID: " + create?.data?.id
+    );
+  } catch (error) {
+    logger.warn("Cant create user transaction" + error);
+  }
+};
--- modules/checkpaymant.js
+++ modules/checkpaymant.js
@@ -1,117 +1,161 @@
 import { phrases } from "../language_ua.js";
 import { bot } from "../app.js";
-import getUserTransactions from '../user-transactions.js';
+import getUserTransactions from "../user-transactions.js";
 import { logger } from "../logger/index.js";
 import { getCardData } from "./checkcardAPI.js";
 import axios from "axios";
+import { createUserTransaction } from "../controllers/createUserTransaction.js";
 
 const checkPayment = async (chatID, deviceId, cardId, phone, user_id) => {
+  setTimeout(async () => {
+    const card = await getCardData(user_id, cardId);
 
-    setTimeout(async () => {
+    console.log(card);
 
-        const card = await getCardData(user_id, cardId);
+    const transaction = await getUserTransactions(deviceId, 4, cardId);
+    console.log(transaction);
+    const paymantAmount =
+      transaction?.cashPaymant ||
+      transaction?.cardPaymant ||
+      transaction?.onlinePaymant ||
+      "null";
 
-        console.log(card);
-        
-        const transaction = await getUserTransactions(deviceId, 4, cardId);
-        console.log(transaction);
-        const paymantAmount = transaction?.cashPaymant||transaction?.cardPaymant||transaction?.onlinePaymant||'null';
-        
-        const balance = card?.WaterQty/10;
-        const deviceData = await axios.post('https://soliton.net.ua/water/api/prices/index.php', 
-            {
-                device_id: deviceId
-            }
+    const balance = card?.WaterQty / 10;
+    const deviceData = await axios.post(
+      "https://soliton.net.ua/water/api/prices/index.php",
+      {
+        device_id: deviceId,
+      }
+    );
+
+    const devicePrices = deviceData.data?.prices;
+
+    const price = devicePrices?.P_1_std / 100;
+
+    const bonus = ((paymantAmount / price) * card?.Discount) / 100;
+
+    if (transaction) {
+      createUserTransaction({
+        device: transaction?.device,
+        date: transaction?.date,
+        waterRequested: transaction?.waterRequested,
+        waterFullfilled: transaction?.waterFullfilled,
+        cashPaymant: transaction?.cashPaymant,
+        cardPaymant: transaction?.cardPaymant,
+        onlinePaymant: transaction?.onlinePaymant,
+        paymantChange: transaction?.paymantChange,
+        cardId: transaction?.cardId,
+      });
+      if (transaction.cardPaymant !== 0) {
+        bot.sendMessage(
+          chatID,
+          `Набрано ${transaction?.waterFullfilled} л. з балансу. 
+Залишок на балансі складає ${balance} л. Дякуємо, пийте на здоров'я`
         );
 
-        const devicePrices = deviceData.data?.prices
+        logger.info(
+          `#️⃣ ${chatID} 📱 ${phone} З балансу карти налито: ${transaction?.waterFullfilled} л.  за ціною ${price} грн/літр`
+        );
+      } else if (
+        transaction.cardPaymant == 0 ||
+        transaction.cashPaymant == 0 ||
+        transaction?.onlinePaymant == 0
+      ) {
+        logger.info(
+          `#️⃣ ${chatID} 📱 ${phone} Активував автомат але не завершив оплату`
+        );
+      } else {
+        bot.sendMessage(
+          chatID,
+          `Внесено: ${paymantAmount} грн, налито: ${transaction?.waterFullfilled} л. за ціною ${price} грн/літр.
+                    Плюс на Ваш баланс нараховано бонус ${bonus} л., загалом баланс складає ${balance} л.. Дякуємо за покупку, пийте на здоров'я 💧`
+        );
 
-        const price = devicePrices?.P_1_std/100;
-
-        const bonus = paymantAmount / price * card?.Discount/100;
-
-        if (transaction) {
-            if (transaction.cardPaymant !== 0) {
-                bot.sendMessage(chatID, `Набрано ${transaction?.waterFullfilled} л. з балансу. 
-Залишок на балансі складає ${balance} л. Дякуємо, пийте на здоров'я`);                    
-                                
-                logger.info(`#️⃣ ${chatID} 📱 ${phone} З балансу карти налито: ${transaction?.waterFullfilled} л.  за ціною ${price} грн/літр`)
-            } else if (transaction.cardPaymant == 0 || transaction.cashPaymant == 0 || transaction?.onlinePaymant == 0) {
-                logger.info(`#️⃣ ${chatID} 📱 ${phone} Активував автомат але не завершив оплату`)
-            } else {
-
-                bot.sendMessage(chatID, `Внесено: ${paymantAmount} грн, налито: ${transaction?.waterFullfilled} л. за ціною ${price} грн/літр.
-                    Плюс на Ваш баланс нараховано бонус ${bonus} л., загалом баланс складає ${balance} л.. Дякуємо за покупку, пийте на здоров'я 💧`);
-                    
-                                
-                logger.info(`#️⃣ ${chatID} 📱 ${phone} Внесено: ${paymantAmount} грн, налито: ${transaction?.waterFullfilled} л. за ціною ${price} грн/літр + бонус ${bonus} л.`)
-                    
-            }
-            
-        } else {
-
-            logger.info(`#️⃣ ${chatID} 📱 ${phone} Активував автомат для оплати готівкою але не завершив оплату`);
-
-        }
-
-    }, 60 * 1000 * 4);
-    
+        logger.info(
+          `#️⃣ ${chatID} 📱 ${phone} Внесено: ${paymantAmount} грн, налито: ${transaction?.waterFullfilled} л. за ціною ${price} грн/літр + бонус ${bonus} л.`
+        );
+      }
+    } else {
+      logger.info(
+        `#️⃣ ${chatID} 📱 ${phone} Активував автомат для оплати готівкою але не завершив оплату`
+      );
+    }
+  }, 60 * 1000 * 4);
 };
 
 const checkPaymentCard = async (chatID, deviceId, cardId, phone, user_id) => {
+  setTimeout(async () => {
+    const card = await getCardData(user_id, cardId);
 
-    setTimeout(async () => {
+    const transaction = await getUserTransactions(deviceId, 4, cardId);
 
-        const card = await getCardData(user_id, cardId);
+    const paymantAmount =
+      transaction?.cashPaymant ||
+      transaction?.cardPaymant ||
+      transaction?.onlinePaymant ||
+      "null";
 
-        const transaction = await getUserTransactions(deviceId, 4, cardId);
-        
-        const paymantAmount = transaction?.cashPaymant||transaction?.cardPaymant||transaction?.onlinePaymant||'null';
-        
-        const balance = card?.WaterQty/10;
-        const deviceData = await axios.post('https://soliton.net.ua/water/api/prices/index.php', 
-            {
-                device_id: deviceId
-            }
+    const balance = card?.WaterQty / 10;
+    const deviceData = await axios.post(
+      "https://soliton.net.ua/water/api/prices/index.php",
+      {
+        device_id: deviceId,
+      }
+    );
+
+    const devicePrices = deviceData.data?.prices;
+
+    const price = devicePrices?.P_1_std / 100;
+
+    const bonus = ((paymantAmount / price) * card?.Discount) / 100;
+
+    if (transaction) {
+      createUserTransaction({
+        device: transaction?.device,
+        date: transaction?.date,
+        waterRequested: transaction?.waterRequested,
+        waterFullfilled: transaction?.waterFullfilled,
+        cashPaymant: transaction?.cashPaymant,
+        cardPaymant: transaction?.cardPaymant,
+        onlinePaymant: transaction?.onlinePaymant,
+        paymantChange: transaction?.paymantChange,
+        cardId: transaction?.cardId,
+      });
+      if (transaction.cardPaymant !== 0) {
+        bot.sendMessage(
+          chatID,
+          `Набрано ${transaction?.waterFullfilled} л. з балансу. 
+Залишок на балансі складає ${balance} л. Дякуємо, пийте на здоров'я`
         );
 
-        const devicePrices = deviceData.data?.prices
+        logger.info(
+          `#️⃣ ${chatID} 📱 ${phone} З балансу карти налито: ${transaction?.waterFullfilled} л.  за ціною ${price} грн/літр`
+        );
+      } else if (
+        transaction.cardPaymant == 0 ||
+        transaction.cashPaymant == 0 ||
+        transaction?.onlinePaymant == 0
+      ) {
+        logger.info(
+          `#️⃣ ${chatID} 📱 ${phone} Активував автомат але не завершив оплату`
+        );
+      } else {
+        bot.sendMessage(
+          chatID,
+          `Внесено: ${paymantAmount} грн, налито: ${transaction?.waterFullfilled} л. за ціною ${price} грн/літр.
+                    Плюс на Ваш баланс нараховано бонус ${bonus} л., загалом баланс складає ${balance} л.. Дякуємо за покупку, пийте на здоров'я 💧`
+        );
 
-        const price = devicePrices?.P_1_std/100;
-
-        const bonus = paymantAmount / price * card?.Discount/100;
-
-        if (transaction) {
-            if (transaction.cardPaymant !== 0) {
-                bot.sendMessage(chatID, `Набрано ${transaction?.waterFullfilled} л. з балансу. 
-Залишок на балансі складає ${balance} л. Дякуємо, пийте на здоров'я`);                    
-                                
-                logger.info(`#️⃣ ${chatID} 📱 ${phone} З балансу карти налито: ${transaction?.waterFullfilled} л.  за ціною ${price} грн/літр`)
-            } else if (transaction.cardPaymant == 0 || transaction.cashPaymant == 0 || transaction?.onlinePaymant == 0) {
-                logger.info(`#️⃣ ${chatID} 📱 ${phone} Активував автомат але не завершив оплату`)
-            } else {
-
-                bot.sendMessage(chatID, `Внесено: ${paymantAmount} грн, налито: ${transaction?.waterFullfilled} л. за ціною ${price} грн/літр.
-                    Плюс на Ваш баланс нараховано бонус ${bonus} л., загалом баланс складає ${balance} л.. Дякуємо за покупку, пийте на здоров'я 💧`);
-                    
-                                
-                logger.info(`#️⃣ ${chatID} 📱 ${phone} Внесено: ${paymantAmount} грн, налито: ${transaction?.waterFullfilled} л. за ціною ${price} грн/літр + бонус ${bonus} л.`)
-                    
-            }
-            
-        } else {
-
-            logger.info(`#️⃣ ${chatID} 📱 ${phone} Активував автомат для оплати з балансу але не завершив оплату`);
-
-        }
-
-    }, 60 * 1000 * 4);
-    
+        logger.info(
+          `#️⃣ ${chatID} 📱 ${phone} Внесено: ${paymantAmount} грн, налито: ${transaction?.waterFullfilled} л. за ціною ${price} грн/літр + бонус ${bonus} л.`
+        );
+      }
+    } else {
+      logger.info(
+        `#️⃣ ${chatID} 📱 ${phone} Активував автомат для оплати з балансу але не завершив оплату`
+      );
+    }
+  }, 60 * 1000 * 4);
 };
 
-
-export {
-    checkPayment,
-    checkPaymentCard
-};
-
+export { checkPayment, checkPaymentCard };
