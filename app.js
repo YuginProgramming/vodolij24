d70484d035adb218ff89400e70a8e8f7d38de89b
brysommer <brysommer1@gmail.com>
2026-03-17 13:33:11 +0200

    greyed qrdecoding

--- app.js
+++ app.js
@@ -1,75 +1,91 @@
-import TelegramBot from 'node-telegram-bot-api';
-import { anketaListiner } from './anketa.js';
-import { dataBot } from './values.js';
-import { decodeQR } from './qrdecode.js';
-import { sequelize } from './models/sequelize.js';
-import getStatistic from './statistic.js';
-import cron from 'node-cron';
-import { botUsersStatistic, botWeeklyUsersStatistic, botMonthlyUsersStatistic } from './modules/statistic/bot-users-statistic.js';
-import mainMenu from './menus/main.js';
-import introduction from './menus/introduction.js';
-import buyWater from './menus/buyWater.js';
-import addToBalance from './menus/addToBalance.js';
-import profile from './menus/profile.js';
+import TelegramBot from "node-telegram-bot-api";
+import { anketaListiner } from "./anketa.js";
+import { dataBot } from "./values.js";
+import { decodeQR } from "./qrdecode.js";
+import { sequelize } from "./models/sequelize.js";
+import getStatistic from "./statistic.js";
+import cron from "node-cron";
+import {
+  botUsersStatistic,
+  botWeeklyUsersStatistic,
+  botMonthlyUsersStatistic,
+} from "./modules/statistic/bot-users-statistic.js";
+import mainMenu from "./menus/main.js";
+import introduction from "./menus/introduction.js";
+import buyWater from "./menus/buyWater.js";
+import addToBalance from "./menus/addToBalance.js";
+import profile from "./menus/profile.js";
 
 const bot = new TelegramBot(dataBot.telegramBotToken, { polling: true });
 
 export { bot };
 
 const main = async () => {
-    const models = {
-        list:  [
-            'users'
-        ]
-    };
-    // DB
-    const configTables = models.list;
-    const dbInterface = sequelize.getQueryInterface();
-    const checks = await Promise.all(configTables.map(configTable => {
-        return dbInterface.tableExists(configTable);
-    }));
-    const result = checks.every(el => el === true);
-    if (!result) {
-        // eslint-disable-next-line no-console
-        console.error(`🚩 Failed to check DB tables, see config.models.list`);
-        throw (`Some DB tables are missing`);
-    }
-}; 
+  const models = {
+    list: ["users"],
+  };
+  // DB
+  const configTables = models.list;
+  const dbInterface = sequelize.getQueryInterface();
+  const checks = await Promise.all(
+    configTables.map((configTable) => {
+      return dbInterface.tableExists(configTable);
+    })
+  );
+  const result = checks.every((el) => el === true);
+  if (!result) {
+    // eslint-disable-next-line no-console
+    console.error(`🚩 Failed to check DB tables, see config.models.list`);
+    throw `Some DB tables are missing`;
+  }
+};
 
 main();
 
-
 anketaListiner();
-decodeQR();
+//decodeQR();
 mainMenu();
 introduction();
 buyWater();
 addToBalance();
 profile();
 
-
 if (dataBot?.statistic === true) {
-    cron.schedule('0 0 * * *', () => {
-        getStatistic();
-    }, {
-        scheduled: true,
-        timezone: 'Europe/Kiev'
-    });
+  cron.schedule(
+    "0 0 * * *",
+    () => {
+      getStatistic();
+    },
+    {
+      scheduled: true,
+      timezone: "Europe/Kiev",
+    }
+  );
 
-    cron.schedule('0 0 * * 0', () => { // Щонеділі о 00:00
-        botWeeklyUsersStatistic();
-    }, {
-        scheduled: true,
-        timezone: 'Europe/Kiev'
-    });
-    
-    cron.schedule('0 0 1 * *', () => { // 1-го числа кожного місяця о 00:00
-        getStatistic();
-        botMonthlyUsersStatistic();
-    }, {
-        scheduled: true,
-        timezone: 'Europe/Kiev'
-    });
+  cron.schedule(
+    "0 0 * * 0",
+    () => {
+      // Щонеділі о 00:00
+      botWeeklyUsersStatistic();
+    },
+    {
+      scheduled: true,
+      timezone: "Europe/Kiev",
+    }
+  );
+
+  cron.schedule(
+    "0 0 1 * *",
+    () => {
+      // 1-го числа кожного місяця о 00:00
+      getStatistic();
+      botMonthlyUsersStatistic();
+    },
+    {
+      scheduled: true,
+      timezone: "Europe/Kiev",
+    }
+  );
 }
 /* не робим щоденну статистику
 cron.schedule('30 0 * * *', () => { //30 хв після опівночі
@@ -79,5 +95,3 @@ cron.schedule('30 0 * * *', () => { //30 хв після опівночі
     timezone: 'Europe/Kiev' 
 });
 */
-
-
