091d7e82a2a1c55edb5046159ef8e8c8013cb146
brysommer <brysommer1@gmail.com>
2026-04-20 14:20:07 +0300

    link price

--- menus/addToBalance.js
+++ menus/addToBalance.js
@@ -7,6 +7,7 @@ import { findUserByChatId, updateUserByChatId } from "../models/users.js";
 import activateDevice from "../modules/activate-device.js";
 import { checkBalanceChange } from "../modules/checkcardAPI.js";
 import { findNearestCoordinate } from "../modules/locations.js";
+import { dataBot } from "../values.js";
 
 const addToBalance = async () => {
   bot.on("message", async (msg) => {
@@ -196,7 +197,7 @@ const addToBalance = async () => {
 
       case "volumeLink":
         if (!isNaN(msg.text)) {
-          const amount = Math.round(msg.text * 2.5);
+          const amount = Math.round(msg.text * dataBot.topUpPrice);
 
           const link = `https://easypay.ua/ua/partners/vodolii1/VODOLII_1_FOP_KMIT-PAY?account=${cardNumber}&amount=${msg.text}`;
           await bot.sendMessage(chatId, phrases.refilInfo, {
